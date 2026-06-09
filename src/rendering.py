c9b6b463b8e5203b1ed043d9a84fd367f4e7c69a
DaniyalKhanGit <daniyalkhan8741@gmail.com>
2026-06-09 14:15:48 -0400

    push current fixes

--- src/rendering.py
+++ src/rendering.py
@@ -1,14 +1,16 @@
 import numpy as np
 import math as mt
 import vortexSolver as vs
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 max_time = 1
 
 sim = vs.FluidSolver(vs.initialGrid())
 for t in range(max_time):
-    vs.FluidSolver.step()
-    plt.plot(vs.FluidSolver.positions, vs.FluidSolver.vorticities)
+    print("start of", t)
+    sim.step()
+    print("step")
+    plt.scatter(sim.positions[:,0], sim.positions[:,1], c=sim.vorticities)
     plt.show()
 
 
--- src/vortexSolver.py
+++ src/vortexSolver.py
@@ -18,75 +18,79 @@ class FluidSolver:
     def __init__(self, particles: tuple, boundary=None):
         self.positions = particles[0]
         self.vorticities = particles[1]
-        self.n_particles = np.size(particles[0])
+        self.n_particles = np.size(particles[1])
         self.boundary = boundary
         self.velocity_field = np.zeros((frame_size**2, 2))
         self.velocity_prev = np.zeros((frame_size**2, 2))
         self.time = 0
 
     # scan through vortons
-    def prepare_solver(new_time: int):
-        FluidSolver.time = new_time
+    def prepare_solver(self, new_time: int):
+        self.time = new_time
 
-        for i in range(FluidSolver.n_particles):
-            if FluidSolver.vorticities[i] == 0:
+        for i in range(self.n_particles):
+            if self.vorticities[i] == -10000:
                 # remove the particle
-                tempVor = FluidSolver.vorticities[i]
-                tempPos = FluidSolver.positions[i]
-                indice = FluidSolver.n_particles - 1
+                tempVor = self.vorticities[i]
+                tempPos = self.positions[i]
+                indice = self.n_particles - 1
 
-                FluidSolver.vorticities[i] = FluidSolver.vorticities[indice]
-                FluidSolver.positions[i] = FluidSolver.vorticities[indice]
-                FluidSolver.vorticities[indice] = 0
-                FluidSolver.positions[indice] = 0
+                self.vorticities[i] = self.vorticities[indice]
+                self.positions[i] = self.positions[indice]
+                self.vorticities[indice] = 0
+                self.positions[indice] = 0
 
-                FluidSolver.n_particles = 0
+                self.n_particles = 0
 
     # also need improvement
-    def compute_velocity_field():
+    def compute_velocity_field(self):
 
-        FluidSolver.velocity_prev = FluidSolver.velocity_field
+        self.velocity_prev = self.velocity_field
 
         # outer loop for each particle in the field
-        for i in range(FluidSolver.n_particles):
-            xth_pos = FluidSolver.positions[i]
+        for i in range(self.n_particles):
+            xth_pos = self.positions[i]
             summation = 0
             
-            for j in range(FluidSolver.n_particles):
+            for j in range(self.n_particles):
                 if j == i:
                     continue
 
-                iX = xth_pos - FluidSolver.positions[j]
+                iX = xth_pos - self.positions[j]
                 iP = abs(iX) / smoothing
-                funcQ = (1 / (2 * mt.pi)) * (1 - mt.exp((-(iP**2)) / 2))
-                circulation = np.cross(iX, FluidSolver.vorticities[j])
-                sum1 = (circulation * funcQ) / (abs(iX)**2)
+                funcQ = (1 / (2 * mt.pi)) * (1 - np.exp((-(iP**2)) / 2))
+                circulation = (-self.vorticities[j] * iX[1], self.vorticities[j] * iX[0])
+                sum1 = (circulation * funcQ) / ((abs(iX)**2) + mt.exp(-100))
                 summation += sum1
-            FluidSolver.velocity_field[i] = -summation
+            self.velocity_field[i] = -summation
 
         return
 
     # need improve
-    def convection():
-        for i in range(FluidSolver.n_particles):
-            xth = FluidSolver.positions[i]
-            xth_vor = FluidSolver.vorticities[i]
+    def convection(self):
+        for i in range(self.n_particles):
+            xth = self.positions[i]
+            xth_vor = self.vorticities[i]
 
-            if (FluidSolver.time == 0):
-                FluidSolver.positions[i] += time_step * FluidSolver.velocity_field[i]
+            if (self.time == 0):
+                self.positions[i] += time_step * self.velocity_field[i]
             else:
-                FluidSolver.positions[i] = xth + (time_step * (((3/2) * FluidSolver.velocity_field[i]) - ((1/2) * FluidSolver.velocity_prev[i])))
+                self.positions[i] = xth + (time_step * (((3/2) * self.velocity_field[i]) - ((1/2) * self.velocity_prev[i])))
 
         return
 
-    def diffusion():
-        FluidSolver.positions += np.random.uniform(0, mt.sqrt(2 * time_step * 0.01))
+    def diffusion(self):
+        self.positions += np.random.uniform(0, mt.sqrt(2 * time_step * 0.01))
 
-    def step():
-        FluidSolver.prepare_solver(FluidSolver.time + time_step)
-        FluidSolver.compute_velocity_field()
-        FluidSolver.convection()
-        FluidSolver.diffusion()
+    def step(self):
+        self.prepare_solver(self.time + time_step)
+        print("prep done")
+        self.compute_velocity_field()
+        print("compute done")
+        self.convection()
+        print("convect done")
+        self.diffusion()
+        print("Diffuse done")
 
 
 # general functions
